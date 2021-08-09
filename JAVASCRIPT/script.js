c2c3eb87d5c90c97df585dbda31f50014f90cf98
Zac <a19860101@gmail.com>
2021-08-09 16:15:40 +0800

    20210809

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,31 @@
+// alert('HELLO WORLD!!');
+// 變數 Varible
+
+var x = 100;
+var y = 999;
+console.log(x,y);
+
+let a = 987;
+let b = 666;
+console.log(a,b);
+/*
+    變數命名規則
+
+    1. 首字必為英文、底線或錢字號
+    2. 大小寫有別
+    3. 符號僅可使用底線與錢字號
+    4. 不可使用保留字
+
+*/
+
+var x = 'hello';
+console.log(x);
+
+let a = 444;
+
+/* 
+    var vs let
+    1. let 不可重複宣告
+    2. var為function scope,let為block scope
+
+*/
