123c3e2f5fb966d034ba92d997d89719d1fc1a57
Zac <a19860101@gmail.com>
2021-08-11 14:52:11 +0800

    20210811

--- JAVASCRIPT/function.js
+++ JAVASCRIPT/function.js
@@ -27,8 +27,52 @@ function user(name){
 
 console.log(user('Macheal'));
 
-function square(w,h){
-    return w * h;
+// function square(w,h){
+//     return w * h;
+// }
+// let square = function(w,h){
+//     return w * h;
+// }
+
+// console.log(square(123,100));
+
+// 箭頭函式
+
+//函式沒有參數
+let a = ()=>{
+    return 'a';
+}
+// 函式有一個參數(可省略小括號)
+let b = b => {
+    return b;
+}
+// 函式有兩個參數以上
+let c = (x,y) => {
+    return x+y;
+}
+// 如果函式的動作只有return,可省略大括號與return
+let d = (i,j) => i*j;
+console.log(d(11,12));
+
+let e = data => data;
+/*
+    let e = function(data){
+        return data;
+    }
+
+*/
+console.log(e(123));
+
+
+let obj = {
+    name: 'john',
+    active: function(){
+        console.log(this);
+    },
+    active2: ()=>{
+        console.log(this);
+    }
 }
 
-console.log(square(123,100));
+obj.active();
+obj.active2();
